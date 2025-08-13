7c65e1d81189f9fecd1c5183b243d2ae3a7c958f
Prasan Singh <prasan.singh@omniva.com>
2025-08-13 12:19:10 -0700

    cleaning up

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,12 +1,15 @@
 module "webserver_cluster" {
-  source = "github.com/isinghprasan/tf_modules//services/webserver-cluster?ref=v0.0.1"
+  source = "../../../../modules/services/webserver-cluster"
 
+  ami = "ami-0fb653ca2d3203ac1"
+  server_text = "Let's see if this changes something"
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-prasan"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
   min_size               = 2
   max_size               = 2
   instance_type          = "t2.micro"
+  enable_autoscaling = false
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
